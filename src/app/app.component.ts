1a0cb8d97df22be44b1ccb31483c8d54e369455d
TayyabSuhail123 <tayyeb.suhail@gmail.com>
2017-06-14 16:30:23 +0500

    New node server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ import {ImageCropperComponent, CropperSettings} from 'ng2-img-cropper';
 <span class="result rounded" *ngIf="data.image" >
     <img [src]="data.image" [width]="cropperSettings.croppedWidth" [height]="cropperSettings.croppedHeight">
 </span>
+
+<router-outlet></router-outlet>
   `
 })
 export class AppComponent {
@@ -23,6 +25,7 @@ cropper:ImageCropperComponent;
     cropperSettings: CropperSettings;
 
     constructor() {
+
 
        debugger;
         
@@ -35,17 +38,20 @@ cropper:ImageCropperComponent;
         this.cropperSettings.cropperDrawSettings.strokeWidth=3;
         this.cropperSettings.cropperDrawSettings.strokeColor="black";
         this.cropperSettings.canvasHeight = 300;
-         this.cropperSettings.noFileInput = true;
+        this.cropperSettings.noFileInput = true;
         this.data = {};
     }
 
-    fileChangeListener($event) {
+
+
+    fileChangeListener() {
+      debugger;
     var image:any = new Image();
     //var file:File = $event.target.files[0];
     //var myReader:FileReader = new FileReader();
     var that = this;
     //myReader.onloadend = function (loadEvent:any) {
-        image.src ="/assets/flower.jpg"
+        image.src ="assets/flower.jpg"
         that.cropper.setImage(image);
 
    // };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,18 +9,5 @@ import { ContactComponent } from './contact/contact.component';
 
 export const rootRouterConfig: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'github', component: RepoBrowserComponent,
-    children: [
-      { path: '', component: RepoListComponent },
-      { path: ':org', component: RepoListComponent,
-        children: [
-          { path: '', component: RepoDetailComponent },
-          { path: ':repo', component: RepoDetailComponent }
-        ]
-      }]
-  },
-  { path: 'contact', component: ContactComponent }
 ];
 
